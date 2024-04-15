e6f3d3d7a029fd7c1fa2b10b68e0362c84232793
Robin Matthew Tsang <robin.tsang@ajacci.com>
2024-04-15 12:22:59 -0500

    changed digital value where counter stops

---
+++ openlane/por_dig/runs/RUN_2024-04-15_08-19-42/05-yosys-jsonheader/_deps.tcl
@@ -0,0 +1,5 @@
+set ::_synlig_defines [list]
+verilog_defines -DUSE_POWER_PINS
+lappend ::_synlig_defines +define+USE_POWER_PINS
+read_liberty -lib -ignore_miss_dir -setattr blackbox /home/rtsang/.volare/volare/sky130/versions/bdc9412b3e468c102d01b7cf6337be06ec6e9c9a/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+set ::env(SYNTH_LIBS) /home/rtsang/chipalooza/sky130_ajc_ip__por/openlane/por_dig/runs/RUN_2024-04-15_08-19-42/tmp/66df82c73676444e9cb04270bb211b29.lib
